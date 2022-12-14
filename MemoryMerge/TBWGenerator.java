4ea05f0ff07130cd6d17ca42f941eed464536c2b
SclafaniDamian <damians@email.sc.edu>
2022-12-14 17:28:36 -0500

    Memory Merge

--- MemoryMerge/TBWGenerator.java
+++ MemoryMerge/TBWGenerator.java
@@ -8,39 +8,225 @@ public class TBWGenerator {
         String result = "";
         switch (rand) {
             case 0:
-                result += "S1. Knight";
+                result += "E1. Trampoline";
                 break;
             case 1:
-                result += "S2. Valkyrie";
+                result += "E2. Sleeping Song";
                 break;
             case 2:
-                result += "S3. Heavy";
+                result += "E3. Zap";
                 break;
             case 3:
-                result += "S4. Archer";
+                result += "E4. Bolt";
                 break;
             case 4:
-                result += "S5. Mage";
+                result += "E5. Lightning Bolt";
                 break;
             case 5:
-                result += "S6. Spy";
+                result += "E6. Booster";
                 break;
             case 6:
-                result += "S7. Shieldman";
+                result += "E7. Mana Pump";
                 break;
             case 7:
-                result += "S8. Shifter";
+                result += "E8. Baseball";
                 break;
             case 8:
-                result += "S9. Bomber";
+                result += "E9. Darkness";
                 break;
             case 9:
-                result += "S10. Basic Giant";
+                result += "E10. Mimic Magic";
                 break;
             case 10:
-                result += "S11. Hatchetman";
+                result += "E11. Bull Rush";
                 break;
             case 11:
+                result += "E12. The Summoning";
+                break;
+            case 12:
+                result += "E13. The Magic Hand";
+                break;
+            case 13:
+                result += "E14. Rat Poison";
+                break;
+            case 14:
+                result += "E15. Pills";
+                break;
+            case 15:
+                result += "E16. Medkit";
+                break;
+            case 16:
+                result += "E17. Bandage";
+                break;
+            case 17:
+                result += "E18. Doc Kit";
+                break;
+            case 18:
+                result += "E19. The Return";
+                break;
+            case 19:
+                result += "E20. Vacuum Cleaner";
+                break;
+            case 20:
+                result += "E21. Gamble";
+                break;
+            case 21:
+                result += "E22. Mega Fertilizer";
+                break;
+            case 22:
+                result += "E23. Photosynthesis";
+                break;
+            case 23:
+                result += "E24. Sticky Bomb";
+                break;
+            case 24:
+                result += "E25. Iron Armor";
+                break;
+            case 25:
+                result += "E26. Steel Armor";
+                break;
+            case 26:
+                result += "E27. Magic Shield";
+                break;
+            case 27:
+                result += "E28. Root Shield";
+                break;
+            case 28:
+                result += "E29. Magnet";
+                break;
+            case 29:
+                result += "E30. Eruption";
+                break;
+            case 30:
+                result += "E31. Coffee Mechine";
+                break;
+            case 31:
+                result += "E32. Green Lightning";
+                break;
+            case 32:
+                result += "E33. EMP Grenade";
+                break;
+            case 33:
+                result += "E34. Negative Energy";
+                break;
+            case 34:
+                result += "E35. Rocket Jets";
+                break;
+            case 35:
+                result += "E36. Teleporter";
+                break;
+            case 36:
+                result += "E37. Creation Spell";
+                break;
+            case 37:
+                result += "E38. Triple Lightning";
+                break;
+            case 38:
+                result += "E39. Big Bomb";
+                break;
+            case 39:
+                result += "E40. Energy Drink";
+                break;
+            case 40:
+                result += "E41. Birdification";
+                break;
+            case 41:
+                result += "E42. Sky Laser";
+                break;
+            case 42:
+                result += "E43. Armory";
+                break;
+            case 43:
+                result += "E44. Sky Light; Storm";
+                break;
+            case 44:
+                result += "E45. Sky Light; Slash";
+                break;
+            case 45:
+                result += "E46. Frost Breath";
+                break;
+            case 46:
+                result += "E47. Training";
+                break;
+            case 47:
+                result += "E48. Atom Bomb";
+                break;
+            case 48:
+                result += "E49. Dance Battle";
+                break;
+            case 49:
+                result += "E50. Sky Light; Ganym";
+                break;
+            case 50:
+                result += "E51. Rescue";
+                break;
+            case 51:
+                result += "E52. Flashlight";
+                break;
+            case 52:
+                result += "E53. Gas Grenade";
+                break;
+            case 53:
+                result += "E54. Magic Malfunction";
+                break;
+            case 54:
+                result += "E55. Furious Winds";
+                break;
+            case 55:
+                result += "E56. Earth Shatter";
+                break;
+            case 56:
+                result += "E57. Ratify";
+                break;
+            case 57:
+                result += "E58. Bag of Rats";
+                break;
+            case 58:
+                result += "E59. Boomerang";
+                break;
+            case 59:
+                result += "E60. Stage";
+                break;
+            case 60:
+                result += "E61. Christmas Gifts";
+                break;
+            case 61:
+                result += "E62. Rocket Fists";
+                break;
+            case 183:
+                result += "S1. Knight";
+                break;
+            case 184:
+                result += "S2. Valkyrie";
+                break;
+            case 185:
+                result += "S3. Heavy";
+                break;
+            case 186:
+                result += "S4. Archer";
+                break;
+            case 187:
+                result += "S5. Mage";
+                break;
+            case 188:
+                result += "S6. Spy";
+                break;
+            case 189:
+                result += "S7. Shieldman";
+                break;
+            case 190:
+                result += "S8. Shifter";
+                break;
+            case 191:
+                result += "S9. Bomber";
+                break;
+            case 192:
+                result += "S10. Basic Giant";
+                break;
+            case 193:
+                result += "S11. Hatchetman";
+                break;
+            case 194:
                 result += "S12. Sparrow";
                 break;
         }
@@ -48,7 +234,7 @@ public class TBWGenerator {
     }
 
     public String generateEquipment() {
-        int rand = (int)(Math.random()*10);     // not complete amount - 183
+        int rand = (int)(Math.random()*62);     // not complete amount - 183
         String result = "";
         switch (rand) {
             case 0:
@@ -80,6 +266,162 @@ public class TBWGenerator {
                 break;
             case 9:
                 result += "E10. Mimic Magic";
+                break;
+            case 10:
+                result += "E11. Bull Rush";
+                break;
+            case 11:
+                result += "E12. The Summoning";
+                break;
+            case 12:
+                result += "E13. The Magic Hand";
+                break;
+            case 13:
+                result += "E14. Rat Poison";
+                break;
+            case 14:
+                result += "E15. Pills";
+                break;
+            case 15:
+                result += "E16. Medkit";
+                break;
+            case 16:
+                result += "E17. Bandage";
+                break;
+            case 17:
+                result += "E18. Doc Kit";
+                break;
+            case 18:
+                result += "E19. The Return";
+                break;
+            case 19:
+                result += "E20. Vacuum Cleaner";
+                break;
+            case 20:
+                result += "E21. Gamble";
+                break;
+            case 21:
+                result += "E22. Mega Fertilizer";
+                break;
+            case 22:
+                result += "E23. Photosynthesis";
+                break;
+            case 23:
+                result += "E24. Sticky Bomb";
+                break;
+            case 24:
+                result += "E25. Iron Armor";
+                break;
+            case 25:
+                result += "E26. Steel Armor";
+                break;
+            case 26:
+                result += "E27. Magic Shield";
+                break;
+            case 27:
+                result += "E28. Root Shield";
+                break;
+            case 28:
+                result += "E29. Magnet";
+                break;
+            case 29:
+                result += "E30. Eruption";
+                break;
+            case 30:
+                result += "E31. Coffee Mechine";
+                break;
+            case 31:
+                result += "E32. Green Lightning";
+                break;
+            case 32:
+                result += "E33. EMP Grenade";
+                break;
+            case 33:
+                result += "E34. Negative Energy";
+                break;
+            case 34:
+                result += "E35. Rocket Jets";
+                break;
+            case 35:
+                result += "E36. Teleporter";
+                break;
+            case 36:
+                result += "E37. Creation Spell";
+                break;
+            case 37:
+                result += "E38. Triple Lightning";
+                break;
+            case 38:
+                result += "E39. Big Bomb";
+                break;
+            case 39:
+                result += "E40. Energy Drink";
+                break;
+            case 40:
+                result += "E41. Birdification";
+                break;
+            case 41:
+                result += "E42. Sky Laser";
+                break;
+            case 42:
+                result += "E43. Armory";
+                break;
+            case 43:
+                result += "E44. Sky Light; Storm";
+                break;
+            case 44:
+                result += "E45. Sky Light; Slash";
+                break;
+            case 45:
+                result += "E46. Frost Breath";
+                break;
+            case 46:
+                result += "E47. Training";
+                break;
+            case 47:
+                result += "E48. Atom Bomb";
+                break;
+            case 48:
+                result += "E49. Dance Battle";
+                break;
+            case 49:
+                result += "E50. Sky Light; Ganym";
+                break;
+            case 50:
+                result += "E51. Rescue";
+                break;
+            case 51:
+                result += "E52. Flashlight";
+                break;
+            case 52:
+                result += "E53. Gas Grenade";
+                break;
+            case 53:
+                result += "E54. Magic Malfunction";
+                break;
+            case 54:
+                result += "E55. Furious Winds";
+                break;
+            case 55:
+                result += "E56. Earth Shatter";
+                break;
+            case 56:
+                result += "E57. Ratify";
+                break;
+            case 57:
+                result += "E58. Bag of Rats";
+                break;
+            case 58:
+                result += "E59. Boomerang";
+                break;
+            case 59:
+                result += "E60. Stage";
+                break;
+            case 60:
+                result += "E61. Christmas Gifts";
+                break;
+            case 61:
+                result += "E62. Rocket Fists";
                 break;
         }
         return result;
